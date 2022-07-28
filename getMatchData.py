8b7207e50f4380c4c36d78e5566e79f7cebb2c92
niketanmoon <niketanmoon@gmail.com>
2022-07-28 10:48:59 +0930

    Formatted code for getting match data and removed invalid games

--- getMatchData.py
+++ getMatchData.py
@@ -1,44 +1,47 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 import io
 import pandas as pd
+import os.path
 
-class MatchData:
-    def __init__(self):
-        self.now = datetime.now()
+def get_match_data():
+    now = datetime.now()
+    aws_path = "https://oracleselixir-downloadable-match-data.s3-us-west-2.amazonaws.com/"
+    year = now.strftime("%Y")
+    current_date = now.strftime("%Y%m%d")
+    yesterday = (now - timedelta(days=1)).strftime("%Y%m%d")
 
-    def get_path(self):
-        # original csv file path
-        # "https://oracleselixir-downloadable-match-data.s3-us-west-2.amazonaws.com/2022_LoL_esports_match_data_from_OraclesElixir_20220721.csv"
-        # generating an automated request url
-        aws_path = "https://oracleselixir-downloadable-match-data.s3-us-west-2.amazonaws.com/"
-
-        # dealing with date patterns
-        year = self.now.strftime("%Y")
-        current_date = self.now.strftime("%Y%m%d")
-
-        name = "_LoL_esports_match_data_from_OraclesElixir_"
-
-        generated_path = f"{aws_path}{year}{name}{current_date}.csv"
-        return generated_path
-
-    def get_data(self, url):
+    directory_name = "csv_files"
+    name = "_LoL_esports_match_data_from_OraclesElixir_"
+    if os.path.isfile(f'{directory_name}/Match Data {current_date}.csv'):
+        match_data = pd.read_csv(f'{directory_name}/Match Data {current_date}.csv', low_memory=False)
+        print("Getting data from todays existing file")
+        return match_data
+    else:
         try:
+            url = f"{aws_path}{year}{name}{current_date}.csv"
             response = requests.get(url, allow_redirects=True)
-            return response.content.decode("utf-8")
+            match_data = response.content.decode("utf-8")
+            match_data = pd.read_csv(io.StringIO(match_data), low_memory=False)
+            assert len(match_data) > 10
+            csv_file_name = f'{directory_name}/Match Data {current_date}.csv'
+            print("Downloaded todays data")
 
-        except Exception as err:
-            print(f"There was error while getting the match data. The error message is {err}")
-
-
-
-data_instance = MatchData()
-url = data_instance.get_path()
-match_data = data_instance.get_data(url)
-
-csv_data = pd.read_csv(io.StringIO(match_data), low_memory=False)
-directory_name = "csv_files/"
-csv_file_name = f'{directory_name}Match Data {datetime.now().strftime("%d_%m_%Y")}.csv'
-# Dumping the dataframe into csv file
-csv_data.to_csv(csv_file_name, index=False)
-
+        except Exception as e:
+            if os.path.isfile(f'{directory_name}/Match Data {yesterday}.csv'):
+                match_data = pd.read_csv(f'{directory_name}/Match Data {yesterday}.csv', low_memory=False)
+                print("Getting data from yesterdays existing file")
+                return match_data
+            else:
+                # if there is no current date data then grab yesterdays data
+                url = f"{aws_path}{year}{name}{yesterday}.csv"
+                response = requests.get(url, allow_redirects=True)
+                match_data = response.content.decode("utf-8")
+                match_data = pd.read_csv(io.StringIO(match_data), low_memory=False)
+                assert len(match_data) > 10
+                csv_file_name = f'{directory_name}/Match Data {yesterday}.csv'
+                print("Downloaded yesterday's data")
+        
+        match_data.to_csv(csv_file_name, index=False)
+        
+        return match_data
